9e02462add5ea4f22ac633a66284cf1a88f213c2
MuhammadArsalan <muhammadarsalanforever@gmail.com>
2024-03-01 13:44:34 +0530

    update

---
+++ app.js
@@ -0,0 +1,50 @@
+// var percentage = +prompt("Enter your Percentage!.");
+
+// if(percentage > 100){
+// console.log("Error !! Invalid Percentage !! ")
+
+// }else if (percentage < 50){
+//     console.log("Percentage =" , percentage , "%"  );
+//     console.log("Grade = Failed");
+    
+// }else if(percentage > 50 && percentage < 60){
+//     console.log("Percentage =" , percentage , "%"  );
+//     console.log("Grade = C");
+// }
+// else if(percentage > 60 && percentage < 70){
+//     console.log("Percentage =" , percentage , "%"  );
+//     console.log("Grade = B");
+// }
+// else if(percentage > 70 && percentage < 80){
+//     console.log("Percentage =" , percentage , "%"  );
+//     console.log("Grade = A");
+// }
+// else(percentage > 80 && percentage <= 100){
+//     console.log("Percentage =" , percentage , "%"  );
+//     console.log("Grade = A-one");
+// }
+
+
+
+
+
+
+
+
+// var input = +prompt("Enter any number !!!")
+
+// if( input %2 === 0){
+//     console.log("It is even number And divisible by 2.. ")
+// }else{
+//    console.log(" It is not an even number and not be dividible by 2...")
+// }
+
+
+
+
+
+
+
+
+
+
